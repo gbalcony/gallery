da9b42798eff8d9465d1fad4b0a98819b05b53f8
George <george@GMB.local>
2023-10-26 12:04:56 +0200

    Add header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ImageService } from './core/services/image.service';
+import { Image } from './core/models/image.model';
+import { AppConfigService } from './core/services/app-config.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'gallery';
+  images: Image[] = [];
+
+  constructor(
+    private imageService: ImageService,
+    private appConfigService: AppConfigService) {}
+
+  ngOnInit(): void {
+    this.images = this.imageService.getImages(this.appConfigService.defaultNumberOfImages);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/layout/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
