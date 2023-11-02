f50605bdaf29ac8a03e471527c8fcd9113a446a2
George <george@GMB.local>
2023-11-02 19:33:25 +0100

    Add image and picsum services tests

--- src/app/core/services/image.service.spec.ts
+++ src/app/core/services/image.service.spec.ts
@@ -1,16 +1,45 @@
 import { TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { ImageService } from './image.service';
+import { HttpClient } from '@angular/common/http';
 
 describe('ImageService', () => {
   let service: ImageService;
+  let httpClient: HttpClient;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+
+    httpClient = TestBed.inject(HttpClient);
     service = TestBed.inject(ImageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return images', () => {
+    const numberOfImages = 3;
+    const expectedResult = [
+      {
+        "id": 1,
+        "photo": "https://picsum.photos/id/1/500/500",
+        "text": "This is the image number 1"
+      },
+      {
+        "id": 2,
+        "photo": "https://picsum.photos/id/2/500/500",
+        "text": "This is the image number 2"
+      },
+      {
+        "id": 3,
+        "photo": "https://picsum.photos/id/3/500/500",
+        "text": "This is the image number 3"
+      }
+    ];
+
+    expect(service.getImages(numberOfImages)).toEqual(expectedResult);
   });
 });
--- src/app/core/services/picsum.service.spec.ts
+++ src/app/core/services/picsum.service.spec.ts
@@ -1,16 +1,65 @@
 import { TestBed } from '@angular/core/testing';
 
 import { PicsumService } from './picsum.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('PicsumService', () => {
   let service: PicsumService;
+  let httpMock: HttpTestingController;
+  const imagesData = [
+    {
+      id: 1,
+      size: {
+        width: 1
+      }
+    },
+    {
+      id: 2,
+      size: {
+        width: 2,
+        height: 2
+      }
+    },
+  ];
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule ]
+    });
     service = TestBed.inject(PicsumService);
+    httpMock = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should return image URL', () => {
+    const expectedResult = [
+      "https://picsum.photos/id/1/1",
+      "https://picsum.photos/id/2/2/2"
+    ];
+    const results: string[] = [];
+
+    imagesData.forEach(imageData => {
+      results.push(service.getImageUrl(imageData.id, imageData.size));
+    });
+
+    expect(results).toEqual(expectedResult);
+  });
+
+  it('should return image blob', () => {
+    const imageData = imagesData[0];
+    
+    
+    service.getImage(imageData.id, imageData.size).subscribe(imageBlob => {
+      expect(imageBlob).toBeDefined();
+    });
+    
+    const req = httpMock.expectOne('https://picsum.photos/id/1/1');
+    expect(req.request.method).toEqual("GET");
+    req.flush(new Blob());
+
+    httpMock.verify();
   });
 });
